5b1030270bfbcc409a6004fc158f7ad70bb03773
nandana-2205 <csnandana2205@gmail.com>
2026-06-22 20:10:14 +0530

    task2

--- task2.py
+++ task2.py
@@ -1,43 +1,38 @@
-def add():
-    a=int(input("Enter first number:"))
-    b=int(input("Enter second number:"))
+def add(a,b):
     print("The sum is:",a+b)
-def sub():
-    a=int(input("Enter first number:"))
-    b=int(input("Enter second number:"))
+def sub(a,b):
     print("The difference is:",a-b)
-def mul():
-    a=int(input("Enter first number:"))
-    b=int(input("Enter second number:"))
+def mul(a,b):
     print("The product is:",a*b)
-def div():
+def div(a,b):
     try:
-        a=int(input("Enter first number:"))
-        b=int(input("Enter second number:"))
         print("The quotient is:",a/b)
         print("The remainder is:",a%b)
     except ZeroDivisionError:
         print("Division by zero is not allowed.")
 while True:
+    print("-----------------------------")
     print("SIMPLE ARITHMATIC OPERATIONS")
-    print("______________________________")
+    print("-----------------------------")
     print("Select the operation you want to perform")
-    print("1.Addition - +")
-    print("2.Subtraction - -")
-    print("3.Multiplication - *")
-    print("4.Division - / and %")
-    print("5.Exit")
-    choice=int(input("Enter your choice:"))
-    if choice==1:
-        add()
-    elif choice==2:
-        sub()
-    elif choice==3:
-        mul()
-    elif choice==4:
-        div()
-    elif choice==5:
+    print("1.Addition (+)")
+    print("2.Subtraction (-)")
+    print("3.Multiplication (*)")
+    print("4.Division (/ and %) ")
+    print("5.End")
+    choice=input("Enter your operation choice: ")
+    a=int(input("Enter the first number: "))
+    b=int(input("Enter the second number: "))
+    if choice=='1' or choice=='+':
+        add(a,b)
+    elif choice=='2' or choice=='-':
+        sub(a,b)
+    elif choice=='3' or choice=='*':
+        mul(a,b)
+    elif choice=='4' or choice=='/':
+        div(a,b)
+    elif choice=='5' or choice.upper()=='END':
         break
     else:
-        print("Invalid choice")
+        print("Invalid operation. Please try again. ")
         
